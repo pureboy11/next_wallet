a8a12fdf77e6d7960b3cacc70cfafd0ccf0899d8
yongseok lee <greatyongseok@gmail.com>
2022-09-06 22:30:34 +0900

    글자 넘어가면 ... 처리 정렬

--- pages/index.js
+++ pages/index.js
@@ -15,20 +15,10 @@ export default function Home() {
     const onChange = (e) => {
         setWalletAddress(e.target.value);
     };
-    const onReset = (e) => {
-        setWalletAddress("");
-    };
-
     const walletType = [""]
     const [loading, setLoading] = useState(true);
 
     console.log(wallets);
-
-
-
-
-
-
 
     return (
         <>
@@ -55,9 +45,6 @@ export default function Home() {
                                     </input>
                                 </Suspense>
                             </div>
-                            <div>
-                            </div>
-
                             <button
                                 className="btn-project rounded text-lg"
                                 type="submit"
@@ -73,54 +60,57 @@ export default function Home() {
                                 }}
                             >
                                 Add
-                                {
+                                {/* {
                                     loading === false ? <MoonLoader
                                         color="#f7eafe"
                                         size={20}
                                         className="ml-3"
                                     /> : null
-                                }
+                                } */}
                             </button>
                         </div>
-
-                        {wallets.length >= 1 ?
-                            wallets.map((a, i) => {
-                                return (
-                                    <div className="flex lg:flex-row md:flex-col mt-5"
-                                        wallets={wallets[i]}
-                                        i={i}
-                                        key={i}
-                                    >
-                                        <div
-                                            type="kaikas"
-                                            className="bg-gray-100 inline-flex py-5 px-2 rounded-lg items-center hover:bg-gray-200 focus:outline-none"
+                        <div className="flex items-baseline w-full ">
+                            {wallets.length >= 1 ?
+                                wallets.map((a, i) => {
+                                    return (
+                                        <div className="flex lg:flex-row md:flex-col mt-5 "
+                                            wallets={wallets[i]}
+                                            i={i}
+                                            key={i}
                                         >
-
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="currentColor"
-                                                className="w-6 h-6"
-                                                viewBox="0 0 512 512"
+                                            <div
+                                                type="kaikas"
+                                                className="bg-gray-100 inline-flex py-5 px-2 rounded-lg items-center hover:bg-gray-200 focus:outline-none "
                                             >
-                                                <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
-                                            </svg>
-                                            <span className="ml-4 flex items-start flex-col leading-none">
-                                                <span className="text-xs text-gray-600 mb-1">{walletType}</span>
-                                                <span className="text-xs text-gray-600 mb-1">{wallets[i]}</span>
-                                            </span>
+
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="currentColor"
+                                                    className="w-6 h-6"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
+                                                </svg>
+                                                <span className="ml-4 flex items-start flex-col leading-none">
+                                                    <span className="text-xs text-gray-600 mb-1 max-w-20 max-h-5">ETH 01</span>
+                                                    <span className="truncate w-40 text-sm ">{wallets[i]}</span>
+                                                </span>
+                                            </div>
+
+
+                                            <HiOutlineX
+                                                className="wallet--x-icon text-black"
+                                                onClick={() => {
+                                                    let copy = [...wallets];
+                                                    copy.splice(i, 1);
+                                                    setWallet(copy);
+                                                }}
+                                            />
                                         </div>
-                                        <HiOutlineX
-                                            className="wallet--x-icon text-black"
-                                            onClick={() => {
-                                                let copy = [...wallets];
-                                                copy.splice(i, 1);
-                                                setWallets(copy);
-                                            }}
-                                        />
-                                    </div>
-                                );
-                            }) : null
-                        }
+                                    );
+                                }) : null
+                            }
+                        </div>
                         {/* <p
                             onClick={() => {
                                 setShow(!show);
