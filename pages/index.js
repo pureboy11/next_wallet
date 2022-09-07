1e7d06b78c2d73cc0473ba7df58a6e85622af981
yongseok lee <greatyongseok@gmail.com>
2022-09-07 15:57:34 +0900

    UseState 정리 스타일 일부 정리

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,12 @@ import { HiOutlineX } from "react-icons/hi";
 
 
 export default function Home() {
-    const [show, setShow] = useState(false);
-    const handleShow = () => setShow(true);
-    const handleClose = () => setShow(false);
     const [walletAddress, setWalletAddress] = useState("");
     const [wallets, setWallet] = useState([]);
     const onChange = (e) => {
         setWalletAddress(e.target.value);
     };
     const walletType = [""]
-    const [loading, setLoading] = useState(true);
-
-    console.log(wallets);
 
     return (
         <>
@@ -56,7 +50,6 @@ export default function Home() {
                                     );
                                     setWallet(copy);
                                     document.getElementById("walletInput").value = '';
-
                                 }}
                             >
                                 Add
@@ -69,46 +62,46 @@ export default function Home() {
                                 } */}
                             </button>
                         </div>
-                        <div className="flex flex-wrap items-baseline ">
-                            {wallets.length >= 1 ?
-                                wallets.map((a, i) => {
-                                    return (
-                                        <div className="flex justify-center  mt-5"
-                                            wallets={wallets[i]}
-                                            i={i}
-                                            key={i}
-                                        >
-                                            <div
-                                                type="kaikas"
-                                                className="flex  py-5 rounded-lg items-center hover:bg-gray-300 focus:outline-none border border-slate-300 shadow-md"
+                        <div className="flex w-full md:justify-start justify-center items-center">
+                            <div className="flex flex-wrap ">
+                                {wallets.length >= 1 ?
+                                    wallets.map((a, i) => {
+                                        return (
+                                            <div className="flex mt-5"
+                                                wallets={wallets[i]}
+                                                i={i}
+                                                key={i}
                                             >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="currentColor"
-                                                    className="w-6 h-6"
-                                                    viewBox="0 0 512 512"
+                                                <div
+                                                    type="kaikas"
+                                                    className="flex bg-gray-100 py-5 rounded-lg items-center hover:bg-gray-300 focus:outline-none border border-slate-300 shadow-md"
                                                 >
-                                                    <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
-                                                </svg>
-                                                <span className="ml-4 flex items-start flex-col leading-none">
-                                                    <span className="text-xs font-mono font-bold text-gray-600">Wallet {i + 1}</span>
-                                                    <span className="truncate text-sm w-20">{wallets[i]}</span>
-                                                </span>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="currentColor"
+                                                        className="w-6 h-6"
+                                                        viewBox="0 0 512 512"
+                                                    >
+                                                        <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
+                                                    </svg>
+                                                    <span className="ml-4 flex items-start flex-col leading-none">
+                                                        <span className="text-sm font-mono font-bold  text-gray-600">Wallet {i + 1}</span>
+                                                        <span className="truncate text-sm w-28 md:w-36 pr-3">{wallets[i]}</span>
+                                                    </span>
+                                                </div>
+                                                <HiOutlineX
+                                                    className="wallet--x-icon"
+                                                    onClick={() => {
+                                                        let copy = [...wallets];
+                                                        copy.splice(i, 1);
+                                                        setWallet(copy);
+                                                    }}
+                                                />
                                             </div>
-
-
-                                            <HiOutlineX
-                                                className="wallet--x-icon text-black"
-                                                onClick={() => {
-                                                    let copy = [...wallets];
-                                                    copy.splice(i, 1);
-                                                    setWallet(copy);
-                                                }}
-                                            />
-                                        </div>
-                                    );
-                                }) : null
-                            }
+                                        );
+                                    }) : null
+                                }
+                            </div>
                         </div>
                         {/* <p
                             onClick={() => {
@@ -124,13 +117,13 @@ export default function Home() {
                                 Check Now
                             </a>
                         </Link>
+
                     </div>
                     <div className="lg:max-w-lg lg:w-1/3 md:w-1/4 w-5/6">
                         <Animation />
                     </div>
                 </div>
             </section>
-
             <style jsx>{``}</style>
         </>
     );
